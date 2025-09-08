cf4f9e934f0a0854f49e6cacb0f9569371540ace
Juan-Pivo10 <juan.goes@estudante.ifms.edu.br>
2025-09-08 08:31:57 -0300

    atividade produtos

--- src/index.ts
+++ src/index.ts
@@ -43,6 +43,49 @@ const conn = await mysql.createConnection({
             res.status(500).send('Erro ao conectar ao banco de dados: ' + error.message)
          }
 })
+app.get('/produtos', async(req, res) => {
+    if(process.env.DBHOST === undefined){
+           res.status(500).send('DBHOST não esta definido nas variaveis de ambiente')
+            return
+        }
+        if(process.env.DBUSER === undefined){
+           res.status(500).send('DBUSER não esta definido nas variaveis de ambiente')
+            return
+        }
+        if(process.env.DBPASSWORD === undefined){
+           res.status(500).send('DBPASSWORD não esta definido nas variaveis de ambiente')
+            return
+        }
+        if(process.env.DBDATABASE === undefined){
+           res.status(500).send('DBDATABASE não esta definido nas variaveis de ambiente')
+            return
+        }
+        if(process.env.DBPORT === undefined){
+           res.status(500).send('DBPORT não esta definido nas variaveis de ambiente')
+            return
+        }
+    try {
+        const conn = await mysql.createConnection({
+            host: process.env.DBHOST,
+            user: process.env.DBUSER,
+            password: process.env.DBPASSWORD,
+            database: process.env.DBDATABASE,
+            port: Number(process.env.DBPORT)
+        })
+        res.send("Conectado ao banco!")
+
+        
+        const [rows] = await conn.query('SELECT * FROM produtos')
+        res.json(rows)
+    } catch (err) {
+        if (err instanceof Error === false) {
+            res.status(500).send('Erro desconhecido ao conectar ao banco de dados')
+            return
+        }
+        const error = err as Error
+        res.status(500).send('Erro ao conectar ao banco de dados: ' + error.message)
+    }
+})
 app.listen(8000, () => {
     console.log('Server is running on port 8000')
 })
